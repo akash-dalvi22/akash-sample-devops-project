32ce489ee8d7edc3a01c0152e432d3e42f6b4df4
AkashArjunDalvi <akashdalvi551@gmail.com>
2024-12-21 10:56:47 +0530

    This is the first version of addition

---
+++ calculator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###########################
+# Author : Akash Dalvi
+# Date : 21/12/2024
+#
+#
+# This script contains calculator logic addition of two numbers
+# Version : V1
+#
+############################
+
+a=2
+b=3
+
+c=a+b
+
